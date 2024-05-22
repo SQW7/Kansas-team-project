c07185fa9c38ef4f8cf07d8be5710b150b7eb80a
SQ W <wsq@SQ-W.local>
2024-05-21 23:02:56 -0700

    landing page

---
+++ src/Kanbas/index.tsx
@@ -0,0 +1,16 @@
+export default function Kanbas() {
+    return (
+        <div id="wd-kanbas-info" className="container">
+            <h1>Kanbas Team Project</h1>
+            <h2>Team Members:</h2>
+            <li>
+                <ul>Shenqian Wen 2024 Summer One</ul>
+                <ul>Xiran Jia 2024 Summer One</ul>
+            </li>
+            <h2>Github Link: </h2>
+            <a href=""> Click Here</a>
+
+
+        </div>
+    );
+}
